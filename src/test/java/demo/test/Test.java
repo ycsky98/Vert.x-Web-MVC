5beb345ba86fa2490de7aea15bbcd8fe5bd7e263
yangcong <690051852@qq.com>
2023-04-19 10:32:22 +0800

    修改代码

--- src/test/java/demo/test/Test.java
+++ src/test/java/demo/test/Test.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.vertx.web.core.router.config.RouterInit;
 import org.vertx.web.spring.JavaBeanContextAnnotation;
 
+import java.io.IOException;
+
 @ComponentScan("demo.test.**")
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Vertx vertx = Vertx.vertx();
         HttpServer httpServer = vertx.createHttpServer();
         httpServer.requestHandler(new RouterInit(new JavaBeanContextAnnotation(Test.class), vertx).getRouter())
